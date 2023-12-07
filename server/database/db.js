1e22f09f7e4e3c7b5371800f50c71cd1f6eca2c2
zeek00 <44764168+zeek00@users.noreply.github.com>
2023-12-07 15:18:10 +0000

    server side code added

---
+++ server/database/db.js
@@ -0,0 +1,20 @@
+// db.js
+const mongoose = require('mongoose');
+
+const uri = process.env.MONGO_URI; // Replace with your MongoDB URI
+
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
+
+module.exports = db;
+
+
--- server/server.js
+++ server/server.js
@@ -1,11 +1,42 @@
+require('dotenv').config();
 const express = require("express");
 const app = express();
+const db = require('./database/db');
+const bodyParser = require('body-parser')
+
+// Middleware to parse incoming JSON requests
+app.use(bodyParser.json())
+
+// Enable trust for proxy
+app.set('trust proxy', 1)
+
+
+// Disable the "X-Powered-By" header
 app.disable("x-powered-by");
 
-// Import express-session below:
-const session = require("express-session")
-const PORT = process.env.PORT || 4001;
+// express-session below:
+const session = require("express-session");
+const MongoStore = require('connect-mongo');
 
+// session middleware below:
+const sessionMiddleware = session({
+  secret: process.env.SECRET,
+  resave: false,
+  cookie: {
+    maxAge: 172800000,
+    secure: process.env.NODE_ENV === 'production',
+    sameSite:'none',
+  },
+  saveUninitialized: false,
+  store: MongoStore.create({
+    mongoUrl: process.env.MONGO_URI, // Provide the MongoDB connection URL
+    touchAfter: 24 * 3600, // Update every 24 hours
+  }),});
+
+app.use(sessionMiddleware);
+
+
+const PORT = process.env.PORT || 4001;
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
 });
