aabe0f88ed40579c1eafe9b7deff5905c0352673
zeek00 <44764168+zeek00@users.noreply.github.com>
2023-12-17 13:26:01 +0000

    extra homepage changrs

--- frontend/src/components/essentials/GoToTop.jsx
+++ frontend/src/components/essentials/GoToTop.jsx
@@ -4,28 +4,35 @@ import styled from 'styled-components';
 
 
 const Top = styled.div`
-    
+  .go-to-top-button{
+    position: absolute;
+    bottom: -230px;
+    font-size: 2.1rem;
+    color: #222;
+    right: 1.7rem; 
+    transition: bottom 0.7s ease-out;
+    cursor: pointer;
+  }
+  .go-to-top-button.visible {
+    bottom: -190px; /* Slide in when visible */
+  }
+
+  .go-to-top-button:hover {
+    color: #dcd0a4;
+    background-color: #333;
+    border-radius: 50%;
+    border-style: none;
+    transition: 0.3s ease;
+
+  }
+  @media only screen and (max-width: 480px) {
     .go-to-top-button{
-        position: absolute;
-        bottom: -230px;
-        font-size: 2.1rem;
-        color: #222;
-        right: 1.7rem; 
-        transition: bottom 0.7s ease-out;
-        cursor: pointer;
+      bottom: -90px;
     }
     .go-to-top-button.visible {
-        bottom: -190px; /* Slide in when visible */
+      bottom: -40px; /* Slide in when visible */
     }
-
-    .go-to-top-button:hover {
-        color: #dcd0a4;
-        background-color: #333;
-        border-radius: 50%;
-        border-style: none;
-        transition: 0.3s ease;
-
-    }
+  }
 
 `;
 
