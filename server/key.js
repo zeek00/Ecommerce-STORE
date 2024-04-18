fba434dfab98d4eca4aa4ea842db8274f3cffdbb
zeek00 <44764168+zeek00@users.noreply.github.com>
2024-04-18 14:43:52 +0100

    deploy.yaml file error fixed

--- server/key.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    mongouri: process.env.MONGOURI,
-    secret: process.env.SECRET,
-    tokenKey: process.env.TOKENKEY,
-    verifaliaUser: process.env.VERIFALIAUSER,
-    verifaliaPass: process.env.VERIFALIAPASS,
-    port: process.env.PORT
-
-};
